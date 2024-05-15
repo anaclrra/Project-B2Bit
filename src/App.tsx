79024abc884ba00c65a88710a0756dff3805b168
anaclrra <anaclarab826@gmail.com>
2024-05-14 22:22:09 -0300

    feat: add routing configuration to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-
-
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import Login from './components/login/Login';
+import UserProfile from './components/profile/UserProfile';
+import './App.css';
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-blue-600'>Hello World!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+  </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
