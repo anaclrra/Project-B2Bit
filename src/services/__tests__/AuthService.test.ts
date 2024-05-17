172301ba79f6384be1b85b18d3ca0d319871f672
anaclrra <anaclarab826@gmail.com>
2024-05-17 18:14:38 -0300

    chore: remove unused directory and files

--- src/services/__tests__/AuthService.test.ts
+++ src/services/__tests__/AuthService.test.ts
@@ -1,33 +1,74 @@
-/* import BrowserRouter  from 'react-router-dom';
-import  Login from '../../components/login/Login';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { describe, expect, test, jest } from '@jest/globals';
+import { User } from '../../types/User';
+import { login } from '../AuthService';
+import nock from 'nock';
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: jest.fn(),
-}));
+// Mocking localStorage
+beforeEach(() => {
+  const localStorageMock = (() => {
+    let store: Record<string, string> = {};
+    return {
+      getItem(key: string) {
+        return store[key] || null;
+      },
+      setItem(key: string, value: string) {
+        store[key] = value;
+      },
+      clear() {
+        store = {};
+      },
+      removeItem(key: string) {
+        delete store[key];
+      }
+    };
+  })();
 
-describe('Login Component', () => {
-  test('submits the form with valid credentials and redirects to profile page', async () => {
-    render(
-      <BrowserRouter>
-        
-      </BrowserRouter>
-    );
-
-   
-    userEvent.type(screen.getByLabelText('E-mail'), 'validemail@example.com');
-    userEvent.type(screen.getByLabelText('Password'), 'validpassword');
-
-    
-    fireEvent.click(screen.getByText('Sign In'));
-
-    
-    await waitFor(() => {
-      expect(window.location.pathname).toEqual('/profile');
-    });
+  Object.defineProperty(global, 'localStorage', {
+    value: localStorageMock,
+    configurable: true,
+    enumerable: true,
+    writable: true
   });
 });
 
- */
+describe('AuthService', () => { 
+    test('should authenticate the user', async () => {
+      nock('https://api.homologation.cliqdrive.com.br')
+        .post('/auth/login/')
+        .reply(200, {
+          tokens: {
+            access: 'tokenAccessValue'
+          }
+        });
+
+      const user: User = { email: 'email@example.com', password: 'password' };
+      const setItemSpy = jest.spyOn(localStorage, 'setItem');
+
+      await login(user);
+
+      expect(setItemSpy).toHaveBeenCalledTimes(1);
+      expect(setItemSpy).toHaveBeenCalledWith('accessToken', 'tokenAccessValue');
+    });
+
+    test('should authenticate the user incorrectly', async () => {
+    
+      nock('https://api.homologation.cliqdrive.com.br', {})
+      .post('/auth/login/')
+      .reply(401, { tokens: { access: 'tokenAccessValue' } } );
+
+      const user: User = {
+      email: 'email@example.com',
+      password: 'password',
+      };
+
+     
+      const setItemSpy = jest.spyOn(localStorage, 'setItem');
+  
+      await expect(login(user)).rejects.toThrow('Erro ao fazer login:');
+      
+      expect(setItemSpy).not.toHaveBeenCalledTimes(1);
+      expect(setItemSpy).not.toHaveBeenCalledWith('accessToken', 'tokenAccessValue');
+      
+      
+  });
+  });
