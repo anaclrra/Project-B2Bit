fc1a1054ca7b220f0ea5b7c6bbce7bb42b1c5aaf
anaclrra <anaclarab826@gmail.com>
2024-05-17 09:02:21 -0300

    style: modify tailwind.config.js to include new themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                '2xl': '0 0 64px 0 rgb(0 0 0 / 0.4)',
+                'md':  '0 2px 10px 0 #0000001A'
+            },
+            spacing: {
+                '96': '22rem',
+              }
+        },
+
     },
     plugins: [],
 };
