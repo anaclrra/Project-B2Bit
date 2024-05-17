1b330ea5d5e040d5f48eb8d33a159e66f526577a
anaclrra <anaclarab826@gmail.com>
2024-05-17 18:10:40 -0300

    style: update tailwind css

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -42,8 +42,8 @@ const UserProfile: React.FC = () => {
       <div className="container w-96 m-auto p-2 mt-20 rounded-2xl shadow-md bg-white">
           <div className='flex flex-col justify-center items-center'>
             <h4 className='text-center font-medium mt-6'>Profile picture</h4>
-            <img src={userProfile.avatar?.medium || defaultAvatar} alt="User Avatar" 
-            className='w-14 h-auto rounded-lg mt-2' />
+            <img src={userProfile.avatar?.high || defaultAvatar} alt="User Avatar" 
+            className='w-20 h-14 rounded-md mt-2' />
           </div>
 
           <div className='mx-3'>
